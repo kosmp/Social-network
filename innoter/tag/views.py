3450a200ad496dc5320060799314a504508a39ba
kosmp <kosmin.2003@mail.ru>
2024-03-02 21:46:57 +0300

    added base implementations of views

--- innoter/tag/views.py
+++ innoter/tag/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Tag
+from .serializers import TagSerializer
+
+
+class TagViewSet(ModelViewSet):
+    queryset = Tag.objects.all()
+
+    serializer_class = TagSerializer
