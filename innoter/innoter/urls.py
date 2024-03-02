3fb3d6d231cac711944fb94fefdd6f8d2afe113f
kosmp <kosmin.2003@mail.ru>
2024-03-02 21:48:57 +0300

    removed route version for admin

--- innoter/innoter/urls.py
+++ innoter/innoter/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("api/v1/admin/", admin.site.urls),
+    path("admin/", admin.site.urls),
     path("api/v1/page/", include("page.urls")),
     path("api/v1", include("post.urls")),
     path("api/v1/tag/", include("tag.urls")),
