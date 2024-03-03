d81b30f33867d106be21998d5e3c1ca72bfbb7e8
kosmp <kosmin.2003@mail.ru>
2024-03-03 14:26:40 +0300

    fixed post path in urls

--- innoter/innoter/urls.py
+++ innoter/innoter/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/page/", include("page.urls")),
-    path("api/v1", include("post.urls")),
+    path("api/v1/", include("post.urls")),
     path("api/v1/tag/", include("tag.urls")),
 ]
