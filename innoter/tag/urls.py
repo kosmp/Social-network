e98e03b1555d40db75a1e8c39c720c2ebd4e9552
kosmp <kosmin.2003@mail.ru>
2024-03-03 13:17:49 +0300

    added filter for list of tags

--- innoter/tag/urls.py
+++ innoter/tag/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import TagViewSet
+from .views import TagsViewSet, TagViewSet
 
 router = DefaultRouter()
-router.register(r"", TagViewSet)
+router.register(r"tag", TagViewSet, basename="tag")
+router.register(r"tags", TagsViewSet, basename="tags")
 
 urlpatterns = router.urls
--- innoter/tag/views.py
+++ innoter/tag/views.py
@@ -1,10 +1,26 @@
-from rest_framework.viewsets import ModelViewSet
+from page.models import Page
+from page.paginations import CustomPageNumberPagination
+from page.serializers import PageSerializer
+from rest_framework import mixins, viewsets
 
 from .models import Tag
 from .serializers import TagSerializer
 
 
-class TagViewSet(ModelViewSet):
+class TagViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Tag.objects.all()
-
     serializer_class = TagSerializer
+
+
+class TagsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    serializer_class = PageSerializer
+    pagination_class = CustomPageNumberPagination
+
+    def get_queryset(self):
+        queryset = Page.objects.all()
+
+        filter_by_name = self.request.query_params.get("filter_by_name")
+        if filter_by_name:
+            queryset = queryset.filter(tags__name=filter_by_name)
+
+        return queryset
