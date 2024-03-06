f05fe07caa6b958caa94f24d8b0664c542ec8ff7
kosmp <kosmin.2003@mail.ru>
2024-03-06 23:57:14 +0300

    added likes_count to PostSerializer. Counter of likes done

--- innoter/post/serializers.py
+++ innoter/post/serializers.py
@@ -1,10 +1,16 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from .models import Post
 
 
 class PostSerializer(ModelSerializer):
+    likes_count = serializers.SerializerMethodField(method_name="get_likes_count")
+
     class Meta:
         model = Post
         fields = "__all__"
         read_only_fields = ("page",)
+
+    def get_likes_count(self, obj):
+        return obj.likes.count()
