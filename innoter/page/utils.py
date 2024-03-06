7f0765ed9abdc60e5037cbd5039c16ae0f0a0470
kosmp <kosmin.2003@mail.ru>
2024-03-06 14:28:21 +0300

    changed get_user_info

--- innoter/page/utils.py
+++ innoter/page/utils.py
@@ -1,22 +1,26 @@
 import os
 
 import jwt
+from jwt import InvalidTokenError
 
 
 def get_user_info(request):
-    token = request.headers.get("Authorization", "").replace("Bearer ", "")
-    decoded = jwt.decode(
-        token,
-        os.environ.get("JWT_SECRET_KEY"),
-        algorithms=[os.environ.get("ALGORITHM")],
-    )
-    user_data = {
-        "user_id": decoded.get("user_id", None),
-        "role": decoded.get("role", None),
-        "group_id": decoded.get("group_id", None),
-        "is_blocked": decoded.get("is_blocked", None),
-        "token_type": decoded.get("token_type", None),
-        "exp": decoded.get("exp", None),
-    }
+    try:
+        token = request.headers.get("Authorization", "").replace("Bearer ", "")
+        decoded = jwt.decode(
+            token,
+            os.environ.get("JWT_SECRET_KEY"),
+            algorithms=[os.environ.get("ALGORITHM")],
+        )
+        user_data = {
+            "user_id": decoded.get("user_id", None),
+            "role": decoded.get("role", None),
+            "group_id": decoded.get("group_id", None),
+            "is_blocked": decoded.get("is_blocked", None),
+            "token_type": decoded.get("token_type", None),
+            "exp": decoded.get("exp", None),
+        }
 
-    return user_data
+        return user_data
+    except (InvalidTokenError, jwt.ExpiredSignatureError) as err:
+        return {}
