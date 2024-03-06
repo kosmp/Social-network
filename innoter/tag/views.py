9acc5a7619481b5847d5375b3fc7a556d244964b
kosmp <kosmin.2003@mail.ru>
2024-03-06 23:13:13 +0300

    added permissions to tag viewsets

--- innoter/tag/views.py
+++ innoter/tag/views.py
@@ -1,5 +1,6 @@
 from page.models import Page
 from page.paginations import CustomPageNumberPagination
+from page.permissions import IsAuthenticated
 from page.serializers import PageSerializer
 from rest_framework import mixins, viewsets
 
@@ -13,10 +14,36 @@ class TagViewSet(
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
+    permission_classes_by_action = {
+        "create": [IsAuthenticated],
+    }
+
+    def get_permissions(self):
+        try:
+            return [
+                permission()
+                for permission in self.permission_classes_by_action[self.action]
+            ]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
+
 
 class TagsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = PageSerializer
     pagination_class = CustomPageNumberPagination
+
+    permission_classes_by_action = {
+        "list": [IsAuthenticated],
+    }
+
+    def get_permissions(self):
+        try:
+            return [
+                permission()
+                for permission in self.permission_classes_by_action[self.action]
+            ]
+        except KeyError:
+            return [permission() for permission in self.permission_classes]
 
     def get_queryset(self):
         queryset = Page.objects.all()
