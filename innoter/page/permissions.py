d4d1bb7b26111d48644e1c56b4bfffdb048cd2a8
kosmp <kosmin.2003@mail.ru>
2024-03-11 21:47:54 +0300

    removed redundant default None from get

--- innoter/page/permissions.py
+++ innoter/page/permissions.py
@@ -6,45 +6,45 @@ class IsAuthenticated(BasePermission):
     def has_permission(self, request, view):
         user_data = get_user_info(request)
         return (
-            user_data.get("user_id", None) is not None
-            and user_data.get("group_id", None) is not None
+            user_data.get("user_id") is not None
+            and user_data.get("group_id") is not None
         )
 
 
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         user_data = get_user_info(request)
-        return user_data.get("role", None) == "admin"
+        return user_data.get("role") == "admin"
 
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
         user_data = get_user_info(request)
-        return user_data.get("role", None) == "moderator"
+        return user_data.get("role") == "moderator"
 
 
 class IsPageOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         user_data = get_user_info(request)
-        return str(obj.user_id) == user_data.get("user_id", None)
+        return str(obj.user_id) == user_data.get("user_id")
 
 
 class IsModeratorOfThePageOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         user_data = get_user_info(request)
         return user_data.get(
-            "role", None
-        ) == "moderator" and obj.owner_group_id == user_data.get("group_id", None)
+            "role"
+        ) == "moderator" and obj.owner_group_id == user_data.get("group_id")
 
 
 class IsAdminOrIsOwnerOrIsModeratorOfTheOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         user_data = get_user_info(request)
         return (
-            user_data.get("role", None) == "admin"
-            or str(obj.user_id) == user_data.get("user_id", None)
-            or user_data.get("role", None) == "moderator"
-            and obj.owner_group_id == user_data.get("group_id", None)
+            user_data.get("role") == "admin"
+            or str(obj.user_id) == user_data.get("user_id")
+            or user_data.get("role") == "moderator"
+            and obj.owner_group_id == user_data.get("group_id")
         )
 
 
@@ -52,7 +52,7 @@ class IsAdminOrIsModeratorOfThePageOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         user_data = get_user_info(request)
         return (
-            user_data.get("role", None) == "admin"
-            or user_data.get("role", None) == "moderator"
-            and obj.owner_group_id == user_data.get("group_id", None)
+            user_data.get("role") == "admin"
+            or user_data.get("role") == "moderator"
+            and obj.owner_group_id == user_data.get("group_id")
         )
