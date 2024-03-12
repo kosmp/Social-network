f624c6f63ed003d40d20e92b385fc723fba3da68
kosmp <kosmin.2003@mail.ru>
2024-03-12 17:41:54 +0300

    uncommented test_patch and test_delete for posts

--- innoter/tests/integration/test_post.py
+++ innoter/tests/integration/test_post.py
@@ -8,16 +8,21 @@ from rest_framework import status
 
 
 class TestPostViewSet:
-    # def test_patch(self, api_client, user_headers, page, post):
-    #     url = f"/api/v1/post/{post.id}/"
-    #     data = {"content": "".join(choices(string.ascii_lowercase, k=5))}
-    #     response = api_client.patch(url, data=json.dumps(data), content_type="application/json", headers=user_headers)
-    #     assert response.status_code == status.HTTP_200_OK
+    def test_patch(self, api_client, user_headers, page, post):
+        url = f"/api/v1/post/{post.id}/"
+        data = {"content": "".join(choices(string.ascii_lowercase, k=5))}
+        response = api_client.patch(
+            url,
+            data=json.dumps(data),
+            content_type="application/json",
+            headers=user_headers,
+        )
+        assert response.status_code == status.HTTP_200_OK
 
-    # def test_delete(self, api_client, user_headers, page, post):
-    #     url = f"/api/v1/post/{post.id}/"
-    #     response = api_client.delete(url, headers=user_headers)
-    #     assert response.status_code == status.HTTP_204_NO_CONTENT
+    def test_delete(self, api_client, user_headers, page, post):
+        url = f"/api/v1/post/{post.id}/"
+        response = api_client.delete(url, headers=user_headers)
+        assert response.status_code == status.HTTP_204_NO_CONTENT
 
     def test_like(self, api_client, user_headers, page, post):
         url = f"/api/v1/post/{post.id}/like/"
