b494ec6e767bbb28d3c314b052e454091ac3a1ca
kosmp <kosmin.2003@mail.ru>
2024-03-13 15:36:10 +0300

    refactored PydanticSettings

--- innoter/innoter/config.py
+++ innoter/innoter/config.py
@@ -4,20 +4,20 @@ from pydantic_settings import BaseSettings
 
 
 class PydanticSettings(BaseSettings):
-    mysql_database: str = None
-    mysql_user: str = None
-    mysql_password: str = None
-    mysql_root_password: str = None
-    mysql_host: str = None
-    mysql_port: int = None
-    jwt_secret_key: str = None
-    algorithm: str = None
-    secret_key: str = None
-    app_port: int = None
-    celery_broker_url: str = None
-    celery_result_backend: str = None
-    logging_filename: str = None
-    logging_to_file_enabled: str = "False"
+    mysql_database: str | None
+    mysql_user: str | None
+    mysql_password: str | None
+    mysql_root_password: str | None
+    mysql_host: str | None
+    mysql_port: int | None
+    jwt_secret_key: str | None
+    algorithm: str | None
+    secret_key: str | None
+    app_port: int | None
+    celery_broker_url: str | None
+    celery_result_backend: str | None
+    logging_filename: str | None
+    logging_to_file_enabled: True | False
 
 
 pydantic_config = PydanticSettings(
